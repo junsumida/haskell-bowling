3b736aff741b1c85444fd0a36b987a83f0ba6b8b
Jun SUMIDA <sjun@wondershake.com>
2013-05-05 12:45:17 +0900

    deleted unnecessary lines and done with strikes calc

--- bowling.hs
+++ bowling.hs
@@ -1,15 +1,12 @@
-import Data.Char
-
 result_perfect   = "XXXXXXXXXXXX"
 result_nums_only = "9-7-4-5-6-2-3-5-7-9-" 
 result_nums_spare = "9/7/4/5-6-2-3-5-7-9-" 
 
---score (x:y:z:xs)
---  | y:z:xs == []  = 0 :: Int
---  | x == '-'  = score y:z:xs
---  | otherwise = digitToInt x + score xs
---score [] = 0 
---score (x:xs) = x : score xs
+score (x:y:z:xs) 
+  | xs == [] && x == 'X' = strike x y z
+  | xs == [] && x /= 'X' = numOrGtr x y z
+  | x == 'X' = strike x y z + score (y:z:xs) 
+  | x /= 'X' = numOrGtr x y z + score xs
 
 resToNum x
   | x == '-'  = 0 :: Int
@@ -23,7 +20,3 @@ numOrGtr x y z
 strike x y z
   | z == '/'  = 20 :: Int 
   | otherwise = 10 + resToNum y + resToNum z
-
-sample = [1,2,3,4]
-sum' (x:[]) = x 
-sum' (x:xs) = x + sum' xs
